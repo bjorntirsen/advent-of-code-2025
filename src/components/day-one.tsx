12b1826227242b376a754b3dcdb67563a9b37790
Björn Tirsén <bjorntirsen@gmail.com>
2025-12-01 22:39:58 +0100

    part two

--- src/components/day-one.tsx
+++ src/components/day-one.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 import { getResultDayOnePartOne } from "./day-one-utils";
-import { inputPartOne } from "./inputPartOne";
+import { inputPartOne } from "./input-part-one";
+import { inputExample } from "./input-example";
+import { getResultDayOnePartTwo } from "./day-one-utils-part-two";
+import { debugInput } from "./debug-input";
 
 export default function DayOne() {
   const [result, setResult] = useState("");
   return (
     <section>
       <div style={{ display: "flex", gap: "1rem" }}>
-        <button onClick={() => setResult(getResultDayOnePartOne())}>
-          Toggle Example Results
+        <button onClick={() => setResult("")}>Reset</button>
+        <button
+          onClick={() => setResult(getResultDayOnePartTwo(debugInput, 0))}
+        >
+          Debug
+        </button>
+        <button onClick={() => setResult(getResultDayOnePartOne(inputExample))}>
+          Show Example Part One Results
         </button>
         <button onClick={() => setResult(getResultDayOnePartOne(inputPartOne))}>
-          Toggle Part One Results
+          Show Part One Results
         </button>
-        <button onClick={() => setResult("")}>Reset</button>
+        <button onClick={() => setResult(getResultDayOnePartTwo(inputExample))}>
+          Show Example Part Two Results
+        </button>
+        <button onClick={() => setResult(getResultDayOnePartTwo("R1000"))}>
+          Show Warning Part Two Results
+        </button>
+        <button onClick={() => setResult(getResultDayOnePartTwo(inputPartOne))}>
+          Show Part Two Results
+        </button>
       </div>
       <pre>{result}</pre>
     </section>

0a11651f6da6f2b24eb91bc8f631b6e4e244bd40
Björn Tirsén <bjorntirsen@gmail.com>
2025-12-03 21:45:29 +0100

    Day three part two

--- src/day-3/getResults.ts
+++ src/day-3/getResults.ts
@@ -1,5 +1,8 @@
 // helpers
-function findTheLargestCombinationOfTwoNumbers(bank: string) {
+function findTheLargestCombinationOfTwoNumbers(bank: string): {
+  totalJoltage: number
+  positions: number[]
+} {
   const arrayOfNumbers = bank.split('').map(Number)
   const arrayOfNumbersExceptLast = arrayOfNumbers.slice(0, -1)
   const largestFirstNumber = Math.max(...arrayOfNumbersExceptLast)
@@ -9,36 +12,81 @@ function findTheLargestCombinationOfTwoNumbers(bank: string) {
   )
   const largestSecondNumber = Math.max(...arrayOfNumbersAfterLargestFirstNumber)
   const indexOfLargestSecondNumber = arrayOfNumbers.indexOf(largestSecondNumber)
+  const totalJoltage = largestFirstNumber * 10 + largestSecondNumber
+  const positions = [indexOfLargestFirstNumber, indexOfLargestSecondNumber]
   return {
-    firstNumber: largestFirstNumber,
-    secondNumber: largestSecondNumber,
-    firstNumberIndex: indexOfLargestFirstNumber,
-    secondNumberIndex: indexOfLargestSecondNumber,
+    totalJoltage,
+    positions,
+  }
+}
+
+function findTheLargestCombinationOfTwelveNumbers(bank: string): {
+  totalJoltage: number
+  positions: number[]
+} {
+  const arrayOfNumbers = bank.split('').map(Number)
+  const totalToPick = 12
+  const picks = new Array(totalToPick).fill(null)
+  interface State {
+    start: number
+    result: number[]
+    positions: number[]
+  }
+  const initialState: State = {
+    start: 0,
+    result: [],
+    positions: [],
+  }
+  const resultingPicks: State = picks.reduce((state, _, pickIndex: number) => {
+    const { start } = state
+    const remaining = totalToPick - pickIndex
+    const end = arrayOfNumbers.length - remaining
+
+    const whereTheMaximumCanBeFound = arrayOfNumbers.slice(start, end + 1)
+
+    const largestNumber = Math.max(...whereTheMaximumCanBeFound)
+
+    const largestNumberIndex = arrayOfNumbers.indexOf(largestNumber, start)
+
+    return {
+      start: largestNumberIndex + 1,
+      result: [...state.result, largestNumber],
+      positions: [...state.positions, largestNumberIndex],
+    }
+  }, initialState)
+
+  const totalJoltage = Number(resultingPicks.result.join(''))
+  const positions = resultingPicks.positions
+
+  return {
+    totalJoltage,
+    positions,
   }
 }
 
 export function getResult(input: string, isPartTwo = false) {
-  console.log('isPartTwo: ', isPartTwo)
-
   function parseInput(input: string) {
     return input.split('\n')
   }
 
   const banks = parseInput(input)
 
-  let result = `Results:\n`
+  let typedOutResult = `Results:\n`
   let totalOutputJoltage = 0
 
   function turnOnBatteries(banks: string[]) {
     banks.forEach((bank) => {
-      const largestCombination = findTheLargestCombinationOfTwoNumbers(bank)
-      const totalJoltage =
-        largestCombination.firstNumber * 10 + largestCombination.secondNumber
+      const { totalJoltage, positions } = isPartTwo
+        ? findTheLargestCombinationOfTwelveNumbers(bank)
+        : findTheLargestCombinationOfTwoNumbers(bank)
+
+      typedOutResult += `- In ${bank}, you can make the largest joltage possible, ${totalJoltage}, by turning on the batteries at positions  ${positions.map((pos: number) => pos + 1).join(', ')}.\n`
+
       totalOutputJoltage += totalJoltage
-      result += `- In ${bank}, you can make the largest joltage possible, ${totalJoltage}, by turning on the batteries at positions ${largestCombination.firstNumberIndex + 1} and ${largestCombination.secondNumberIndex + 1}.\n`
     })
   }
+
   turnOnBatteries(banks)
-  result += `\nThe total output joltage of all banks is ${totalOutputJoltage}.\n`
-  return result
+  typedOutResult += `\nThe total output joltage of all banks is ${totalOutputJoltage}.\n`
+  return typedOutResult
 }

b430fbb83b017fbece349a4bfd7d3d0fc470d18b
Björn Tirsén <bjorntirsen@gmail.com>
2025-12-03 19:42:35 +0100

    Add day one tests

--- src/day-1/day-one.test.ts
+++ src/day-1/day-one.test.ts
@@ -1,7 +1,23 @@
 import { describe, expect, test } from 'vitest'
+import { getResultDayOnePartOne } from './day-one-utils-part-one'
+import { inputExample } from './input-example'
+import { getResultDayOnePartTwo } from './day-one-utils-part-two'
+import { inputPartOne } from './input-part-one'
 
-describe('day one tests', () => {
-  test('sample test', () => {
-    expect(1 + 1).toBe(2)
+describe('day one', () => {
+  test('example', () => {
+    expect(getResultDayOnePartOne(inputExample)).toContain(
+      'the password in this example is 3',
+    )
+  })
+  test('part one', () => {
+    expect(getResultDayOnePartOne(inputPartOne)).toContain(
+      'the password in this example is 1064.',
+    )
+  })
+  test('part two', () => {
+    expect(getResultDayOnePartTwo(inputPartOne)).toContain(
+      'the password in this example is 6122.',
+    )
   })
 })

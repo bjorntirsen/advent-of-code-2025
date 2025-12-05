1b165d4f82402674d5c2cdd197e66a1b61d57ad1
Björn Tirsén <bjorn.tirsen@sbab.se>
2025-12-05 17:58:51 +0100

    Solve day five part two

--- src/day-5/getResults.test.ts
+++ src/day-5/getResults.test.ts
@@ -3,7 +3,7 @@ import { getResult } from './getResults'
 import { exampleInput } from './day-five'
 import input from './input.txt?raw'
 
-describe('Day Four', () => {
+describe('Day Five', () => {
   describe('Part One', () => {
     it('example', () => {
       expect(getResult(exampleInput)).toContain(
@@ -13,6 +13,18 @@ describe('Day Four', () => {
     it('input', () => {
       expect(getResult(input)).toContain(
         '733 of the available ingredient IDs are fresh',
+      )
+    })
+  })
+  describe('Part Two', () => {
+    it('example', () => {
+      expect(getResult(exampleInput, true)).toContain(
+        'the fresh ingredient ID ranges consider a total of 14 ingredient IDs to be fresh',
+      )
+    })
+    it('input', () => {
+      expect(getResult(input, true)).toContain(
+        'the fresh ingredient ID ranges consider a total of 345821388687084 ingredient IDs to be fresh',
       )
     })
   })

d8db893ac97cdbd875e59100a0755bacfc250b46
Björn Tirsén <bjorntirsen@gmail.com>
2025-12-20 18:10:10 +0100

    Add prettier and modal

--- src/day-1/day-one.tsx
+++ src/day-1/day-one.tsx
@@ -4,13 +4,21 @@ import { inputPartOne } from './input-part-one'
 import { inputExample } from './input-example'
 import { getResultDayOnePartTwo } from './day-one-utils-part-two'
 import { debugInput } from './debug-input'
+import { Modal } from '../ui/modal'
 
 export default function DayOne() {
   const [result, setResult] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <section>
       <h2>Day 1: Secret Entrance</h2>
       <div className="section-body">
+        <button onClick={() => setIsModalOpen(true)}>
+          Visualize Part One Example
+        </button>
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          This is the content of the modal
+        </Modal>
         <button onClick={() => setResult('')}>Reset</button>
         <button
           onClick={() => setResult(getResultDayOnePartTwo(debugInput, 0))}
